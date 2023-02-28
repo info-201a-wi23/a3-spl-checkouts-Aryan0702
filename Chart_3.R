2be4b2be73d348d99aac0bc9778580a85eb73930
Aryan Auti <aauti@uw.edu>
2023-02-27 21:14:27 -0800

    Final changes to chart_3.r

--- Chart_3.R
+++ Chart_3.R
@@ -1,4 +1,4 @@
 library("ggplot2")
 
 ggplot(data = rick_riordan_df3) + 
-  geom_col(mapping = aes(y = as.numeric(Checkouts), x = PublicationYear, fill = MaterialType)) + labs(y = "Monthly Checkouts", x = "Publication Year", title = "Post 2020 Checkouts of Each publication Year")
+  geom_col(mapping = aes(y = as.numeric(Checkouts), x = PublicationYear, fill = MaterialType)) + labs(y = "Monthly Checkouts", x = "Publication Year", title = "Post 2020 Checkouts of Each publication Year") + theme(axis.text.x = element_text(size = 7, angle = 45, hjust = 1))
