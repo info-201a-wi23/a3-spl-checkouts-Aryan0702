2ca2c53f34349590f2f412415c6547b456034adc
Aryan Auti <aauti@uw.edu>
2023-02-27 17:07:54 -0800

    index.rmd updated

--- Chart_3.R
+++ Chart_3.R
@@ -1,4 +1,4 @@
 library("ggplot2")
 
 ggplot(data = rick_riordan_df3) + 
-  geom_col(aes(y = as.numeric(Checkouts), x = PublicationYear, fill=CheckoutMonth)) + labs(y = "Monthly Checkouts", x = "Publication Year", title = "Total Checkouts of Each publication Year")
+  geom_col(mapping = aes(y = as.numeric(Checkouts), x = PublicationYear, fill = MaterialType)) + labs(y = "Monthly Checkouts", x = "Publication Year", title = "Post 2020 Checkouts of Each publication Year")
--- Data_Explored.R
+++ Data_Explored.R
@@ -4,7 +4,7 @@ library("tidyverse")
 library("ggplot2")
 library(tidyr)
 
-checkouts_17_23 <- read.csv("~/Desktop/College Books/Win 23/INFO 201/Assignment/a3-spl-checkouts-Aryan0702/2017-2023-10-Checkouts-SPL-Data.csv", stringsAsFactors = FALSE)
+checkouts_17_23 <- read.csv("/Users/autia/Desktop/2017-2023-10-Checkouts-SPL-Data.csv", stringsAsFactors = FALSE)
 
 rick_riordan_df <- checkouts_17_23 %>% filter(str_detect(Creator, "(?i)riordan"))
 
@@ -13,6 +13,8 @@ rick_riordan_df$PublicationYear <- gsub("\\D", "", rick_riordan_df$PublicationYe
 rick_riordan_df1 <- rick_riordan_df %>% group_by(MaterialType, PublicationYear) %>% summarise(Checkouts = sum(Checkouts))
 
 rick_riordan_df2 <- rick_riordan_df %>% group_by(PublicationYear, CheckoutMonth)%>% summarise(Checkouts = sum(Checkouts))
+rick_riordan_df2 <- rick_riordan_df2 %>% filter(PublicationYear > 2012)
 
-rick_riordan_df3 <- rick_riordan_df %>% group_by(PublicationYear, CheckoutMonth)%>% summarise(Checkouts = sum(Checkouts))
+rick_riordan_df3 <- rick_riordan_df %>% group_by(PublicationYear, CheckoutMonth, CheckoutYear, MaterialType) %>% summarise(Checkouts = sum(Checkouts))
+rick_riordan_df3 <- rick_riordan_df3 %>% filter(CheckoutYear >= 2020)
 
